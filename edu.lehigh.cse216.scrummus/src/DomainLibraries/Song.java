8ba617da746683847916e011958942e3ec5b2927
Zachary Daniels <zachdaniels@dyn154058.cc.lehigh.edu>
2012-11-08 22:25:21 -0500

    Updated Song class w/ constructors and accessors and mutators

--- edu.lehigh.cse216.scrummus/src/DomainLibraries/Song.java
+++ edu.lehigh.cse216.scrummus/src/DomainLibraries/Song.java
@@ -9,10 +9,49 @@ package DomainLibraries;
  * @author zachdaniels
  */
 public class Song {
-    String artist;
-    String album;
-    String lyrics;
-    int albumNumber;
+    private String artist;
+    private String album;
+    private String lyrics;
+    private int albumNumber;
+    
+    public Song(String artist, String album, String lyrics, int albumNumber){
+        this.artist = artist;
+        this.album = album;
+        this.lyrics = lyrics;
+        this.albumNumber = albumNumber;
+    }
+    
+    public String getArtist(){
+        return artist;
+    }
+    
+    public void setArtist(String artist){
+        this.artist = artist;
+    }
+    
+    public String getAlbum(){
+        return album;
+    }
+    
+    public void setAlbum(String album){
+        this.album = album;
+    }
+    
+    public String getLyrics(){
+        return lyrics;
+    }
+    
+    public void setLyrics(String lyrics){
+        this.lyrics = lyrics;
+    }
+    
+    public int getAlbumNumber(){
+        return albumNumber;
+    }
+    
+    public void setAlbumNumber(int albumNumber){
+        this.albumNumber = albumNumber;
+    }
     
     @Override
     public String toString(){
